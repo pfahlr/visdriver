3eb5162663b79569546a421fe51bf3a806c30caf
Rick Pfahl <pfahlr@gmail.com>
2025-10-04 00:25:21 -0400

    Add AVS debug instrumentation and offscreen capture

---
+++ tools/debug_trace.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <windows.h>
+
+bool DebugTraceInitialize(const std::wstring &log_path);
+void DebugTraceShutdown();
+
+void DebugTraceLog(const wchar_t *format, ...);
+void DebugTraceLog(const std::wstring &message);
+
+bool DebugTraceInstallHooksForModule(HMODULE module,
+                                     const std::wstring &module_name);
+
+void DebugTraceRegisterTargetWindow(HWND hwnd);
+void DebugTraceUnregisterTargetWindow(HWND hwnd);
+
+bool DebugTraceConfigureOffscreenSurface(int width, int height);
+void DebugTraceResetOffscreenSurface();
+
+bool DebugTraceCaptureOffscreenSurface(std::vector<uint8_t> &out_rgba);
+
+bool DebugTraceIsActive();
+
